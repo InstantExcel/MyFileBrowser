a9a870d2020515c05b20ac9121886cc848d0c556
John Martin <therealjohnnysix@gmail.com>
2021-11-28 23:57:18 +0000

    Updages..

--- MyFileBrowser/MainForm.cs
+++ MyFileBrowser/MainForm.cs
@@ -26,6 +26,7 @@ namespace MyFileBrowser
         public int MyFolderSequenceCount = 0;
         public List<string> MyFilesRAW = new List<string>();
         public List<string> MyFilesShort = new List<string>();
+        public List<int> MyFilesFrameCount= new List<int>();
 
 
         public MainForm()
@@ -39,26 +40,35 @@ namespace MyFileBrowser
         //----------------------------------------------------
         public int GetFileNameIndex(string FileNameIn, Array MyArray)
         {
+            // SEARCH THROUGH LSIT TO GET MATCHING INDEX
 
-            return -1;
+            if (MyFilesShort.Count > 0 && bHasFolderBeenPicked)
+            {
+
+            }
+            else
+            {
+                
+                return -1; //ERRPR
+            }
+
+                
         }
 
         //-------------------------------------------------------
         // :: GET JUST TEXT PORTION, MASK NUMBER AS XXXX
-        //----------------------------------------------------
+        //-------------------------------------------------------
 
         public String FileNameCleaned(string FilenameIn)
         {
             string TempString;
-
-            // Nothing::: YES!!
             TempString = Regex.Replace(FilenameIn, "[0-9]", string.Empty);
 
             return TempString;
         }
-        //--------------------------------------------------
+        //----------------------------------------------------
         // GET JUST NUMBER PORTION OF FILENAME ::
-        //---------------------------------------------------
+        //-----------------------------------------------------
         public Int32 FileNameNumeric(string FilenameIn)
         {
             string TempString;
@@ -76,19 +86,29 @@ namespace MyFileBrowser
         {
             ChooseFolder();
         }
-        // PICK FOLDER::
+        
+        //--------------------------------------------------------------
+        // ::  Pick folder, store data into lists :::::::::::::::::::::
+        //--------------------------------------------------------------
         public void ChooseFolder()
         {
             if (MyFolderBrowser.ShowDialog() == DialogResult.OK)
             {
                 string MyPath = MyFolderBrowser.SelectedPath;
                 MyFolderBox.Text = MyPath;
+                // :: TEMP  -  Filenames into a csv list ?
+
                 big_list_box.Text = GetFolderFilesIntoString(MyPath);
 
                 MyFilesRAW = new List<string>(big_list_box.Text.Split(','));
-                // Do list of image sequences found.. 
+                
+                //-----------------------------------------
+                // :: Do list of image sequences found.. ::
+                //------------------------------------------
+                
                 DoCompressedList();
 
+                // :: Output / debug test... 
 
                 groupBox1.Text = "Folder " + MyPath + " searched @ " + DateTime.Now.ToString() + " found  " + MyFolderFileCount.ToString() + " items.";
                 
@@ -129,34 +149,41 @@ namespace MyFileBrowser
 
         public void DoCompressedList()
         {
-            string LastName;
-            string CurrentName;
-            int j = 0; // Iterator for Sequence block count :: 
-            for (var i = 0; i < MyFilesRAW.Count; i++)
+            string LastName="";
+            string CurrentName ;
+            int k = 1; // ::: Count of potential frames in sequence - always start with 1 :::
+            int j = 0; // ::: Iterator for Sequence block count :::
+            for (var i = 0; i < MyFilesRAW.Count-1; i++)
             {
-                if (i == 0) {
-                    LastName = MyFilesRAW[i];
-                    MyFilesShort.Add( FileNameCleaned(MyFilesRAW[i]));
-                }
+                CurrentName = FileNameCleaned(MyFilesRAW[i]);
 
+                // If first in Sequence 
+                if (i == 0) 
+                {
+                    MyFilesShort.Add(CurrentName);
+                }
+                // :: If name different to last row.  ::
+                
                 if (i > 0)
                 {
-                    CurrentName = MyFilesRAW[i];
-                    MyFilesShort[j] = FileNameCleaned(MyFilesRAW[i]);
-                    j += 1;
+                    if (CurrentName != LastName)
+                    {
+                        MyFilesShort.Add(CurrentName);
+                        j += 1;
+                        MyFilesFrameCount.Add(k); // FRAME COUNT??  
+                        k = 1;
+                    }
+                    
                 }
-
-                if (i > 0 && MyFilesRAW[i] != MyFilesRAW[i - 1])
-                {
-                    LastName = MyFilesRAW[i];
-                    CurrentName = MyFilesRAW[i];
-                }
-
+                k = k + 1;
+                // :: Store Current to 'last' for compare next iteration ::
+                LastName = FileNameCleaned(MyFilesRAW[i]);
 
             }
+
             for (var i = 0; i < MyFilesShort.Count; i++)
             {
-                DebugOutBox.AppendText(MyFilesShort[i]);
+                DebugOutBox.AppendText(MyFilesShort[i] + " Was "+ MyFilesFrameCount[i] + " frames??");
             }
             MyFolderSequenceCount = j;
             MessageBox.Show("J is " + j.ToString(), "aaa");
